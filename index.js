662e8c5ca848ba9f344902e4cef0c3e42350d608
Eduard Musatov <pkboyar09@gmail.com>
2026-02-17 21:51:37 +0300

    init

---
+++ index.js
@@ -0,0 +1,336 @@
+import fs from 'fs';
+import path from 'path';
+
+const semanticFields = [
+  'food & cooking',
+  'programming & it',
+  'economics',
+  'law & crime',
+  'anatomy & health',
+  'animals',
+];
+
+const partsOfSpeech = [
+  'noun',
+  'verb',
+  'adjective',
+  'phrase',
+  'collocation',
+  'phrasal verb',
+  'adverb',
+  'contraction',
+  'preposition',
+  'union',
+  'pronoun',
+  'sentence',
+];
+const months = {
+  Jan: 0,
+  Feb: 1,
+  Mar: 2,
+  Apr: 3,
+  May: 4,
+  June: 5,
+  July: 6,
+  Aug: 7,
+  Sep: 8,
+  Oct: 9,
+  Nov: 10,
+  Dec: 11,
+};
+const numberedEntryRe = /^\d+\)/; // начинаем с цифры и закрывающей в конце скобки
+const russianLetterEntryRe = /^[А-Яа-яЁё]\)/; // начинаем с одной буквы из русского алфавита и закрывающей в конце скобки
+const translationAndMeaningRe = /\)\s*[—–-−-]\s*(.+)$/; // находим ), любые пробелы, любый тип тире/дефиса/минуса, любые пробелы, берем все до конца строки
+
+// находит все двойные кавычки и заменяют каждую на две кавычки
+function escapeCsvQuotes(value) {
+  if (value == null) return '';
+  return `${value.replace(/"/g, '""')}`;
+}
+
+function toLocalISOString(date) {
+  const pad = (n) => {
+    return n.toString().padStart(2, '0');
+  };
+
+  const year = date.getFullYear();
+  const month = pad(date.getMonth() + 1);
+  const day = pad(date.getDate());
+
+  return `${year}-${month}-${day}`;
+}
+
+function getDateFromLine(line) {
+  let dateString = '';
+  if (line[1] !== ' ') {
+    dateString = line.substring(1);
+  } else {
+    dateString = line.substring(2);
+  }
+
+  const dateArray = dateString.split(' ');
+  const date = Number(dateArray[0]); // автоматически преобразовывает строку типа 02 в нормальное число
+  const month = months[dateArray[1]];
+  const year = Number(dateArray[2]);
+
+  return new Date(year, month, date, 0, 0, 0, 0);
+}
+
+// TODO: также добавить __ (тоже жирный текст)
+function replaceMarkdownDelimiter(line, delimiter) {
+  if (!['**', '*', '_'].includes(delimiter)) {
+    throw new Error('markdown delimiter invalid format');
+  }
+
+  let newLine = '';
+  let delimCounter = 0;
+  let firstDelimPos = -1;
+  let tagName = delimiter === '**' ? 'b' : 'i';
+
+  const isDouble = delimiter.length === 2; // delimiter === **
+
+  for (let i = 0; i < line.length; i++) {
+    if (
+      isDouble ? line[i] === '*' && line[i + 1] === '*' : line[i] === delimiter
+    ) {
+      delimCounter += 1;
+
+      if (delimCounter === 1) {
+        firstDelimPos = newLine.length;
+      } else if (delimCounter === 2) {
+        newLine =
+          newLine.slice(0, firstDelimPos) +
+          `<${tagName}>` +
+          newLine.slice(firstDelimPos);
+        newLine += `</${tagName}>`;
+
+        delimCounter = 0;
+        firstDelimPos = -1;
+      }
+
+      if (delimiter === '**') {
+        i++; // пропускаем второй символ *
+      }
+    } else {
+      newLine += line[i];
+    }
+  }
+
+  if (delimCounter === 1 && firstDelimPos !== -1) {
+    newLine =
+      newLine.slice(0, firstDelimPos) +
+      delimiter +
+      newLine.slice(firstDelimPos);
+  }
+
+  return newLine;
+}
+
+function convertMarkdownToHTML(line) {
+  const hasBoldDelimiter = line.includes('**');
+  const hasItalicsDelimiter = line.includes('_');
+  if (!hasBoldDelimiter && !hasItalicsDelimiter) {
+    return line;
+  }
+
+  let newLine = line;
+  if (hasBoldDelimiter) {
+    newLine = replaceMarkdownDelimiter(line, '**');
+  }
+  if (hasItalicsDelimiter) {
+    newLine = replaceMarkdownDelimiter(newLine, '_');
+  }
+  return newLine;
+}
+
+function removeMarkdownDelimiters(text) {
+  return text.replace(/[*_]+/g, '');
+}
+
+function splitTranslationAndMeaning(markdownText) {
+  const text = removeMarkdownDelimiters(markdownText);
+
+  const parts = text.split(',').map((p) => p.trim());
+
+  let translationParts = [];
+  let meaningParts = [];
+
+  let meaningStarted = false;
+
+  for (const part of parts) {
+    if (!meaningStarted && /^[А-Яа-яЁё]/.test(part)) {
+      // проверяет, начинается ли с русской буквы
+      translationParts.push(part);
+    } else {
+      meaningStarted = true;
+      meaningParts.push(part);
+    }
+  }
+
+  return {
+    translation: translationParts.join(', '),
+    meaning: meaningParts.join(', '),
+  };
+}
+
+function createCsvLine(csvItem) {
+  return (
+    [
+      toLocalISOString(csvItem.createdDate),
+      `"${escapeCsvQuotes(csvItem.word)}"`,
+      csvItem.partOfSpeech,
+      `"${escapeCsvQuotes(csvItem.translation)}"`,
+      `"${escapeCsvQuotes(csvItem.meaning)}"`,
+      `"${escapeCsvQuotes(csvItem.comment)}"`,
+    ].join(';') + '\n'
+  );
+}
+
+function getTextAfterClosingParenthesis(text) {
+  return text.substring(text.indexOf(')') + 1);
+}
+
+// structure: createdDate | word | partOfSpeech | translation | meaning | comment
+let csvString = '';
+
+let unparsedWordsString = '';
+let unparsedWordsCount = 0;
+
+const inputPath = path.resolve('input/vocab.md');
+const mdFile = fs.readFileSync(inputPath);
+const mdFileString = mdFile.toString();
+const mdFileLines = mdFileString.split(/\r?\n/).filter(Boolean);
+
+const csvItem = {
+  createdDate: new Date(),
+  word: '',
+  partOfSpeech: '',
+  translation: '',
+  meaning: '',
+  comment: '',
+};
+let createdDate = new Date();
+let startOfWordIdx = 0;
+
+for (let i = 0; i < mdFileLines.length; i++) {
+  const line = mdFileLines[i];
+
+  if (line.startsWith('#')) {
+    // TODO: обрабатывать как-то ошибки тут. Что мы должны делать, если строка с датой неправильная?
+    createdDate = getDateFromLine(line);
+
+    continue;
+  }
+
+  try {
+    if (numberedEntryRe.test(line)) {
+      startOfWordIdx = i;
+
+      // TODO: думаю, странное условие
+      if (csvItem.word !== '' && csvItem.partOfSpeech !== '') {
+        csvString += createCsvLine(csvItem);
+
+        csvItem.createdDate = new Date();
+        csvItem.word = '';
+        csvItem.partOfSpeech = '';
+        csvItem.translation = '';
+        csvItem.meaning = '';
+        csvItem.comment = '';
+      }
+
+      const lineContent = getTextAfterClosingParenthesis(line);
+
+      let word = lineContent.substring(0, lineContent.indexOf('(')).trimEnd();
+
+      const partOfSpeech = lineContent.substring(
+        lineContent.indexOf('(') + 1,
+        lineContent.indexOf(')'),
+      );
+      if (!partsOfSpeech.includes(partOfSpeech)) {
+        throw new Error('partOfSpeech');
+      }
+
+      if (partOfSpeech === 'verb' && !word.startsWith('to')) {
+        word = `to ${word}`;
+      }
+
+      const match = lineContent.match(translationAndMeaningRe);
+      const hasRussianEntry = russianLetterEntryRe.test(mdFileLines[i + 1]);
+      // если нету перевода и нету на следующей строке варианта перевода || если есть перевод и есть варинат перевода на следующей строке
+      if ((!match && !hasRussianEntry) || (match && hasRussianEntry)) {
+        throw new Error('translationAndMeaning');
+      }
+      if (!match) {
+        csvItem.createdDate = createdDate;
+        csvItem.word = word;
+        csvItem.partOfSpeech = partOfSpeech;
+        continue;
+      }
+
+      const translationAndMeaning = match[1];
+      const { translation, meaning } = splitTranslationAndMeaning(
+        translationAndMeaning,
+      );
+
+      csvItem.createdDate = createdDate;
+      csvItem.word = word;
+      csvItem.partOfSpeech = partOfSpeech;
+      csvItem.translation = translation;
+      csvItem.meaning = meaning;
+    } else if (russianLetterEntryRe.test(line)) {
+      if (line[0] != 'А') {
+        // второй или более вариант перевода
+        csvString += createCsvLine(csvItem);
+
+        csvItem.translation = '';
+        csvItem.meaning = '';
+        csvItem.comment = '';
+      }
+
+      const lineContent = getTextAfterClosingParenthesis(line);
+      if (!lineContent) {
+        throw new Error('translationAndMeaning');
+      }
+      const { translation, meaning } = splitTranslationAndMeaning(lineContent);
+
+      csvItem.translation = translation;
+      csvItem.meaning = meaning;
+    } else {
+      csvItem.comment += `${csvItem.comment ? '<br>' : ''}${convertMarkdownToHTML(line)}`;
+    }
+  } catch (e) {
+    unparsedWordsCount += 1;
+
+    const dateString = `# ${createdDate.getDate()} ${Object.keys(months)[createdDate.getMonth()]} ${createdDate.getFullYear()}`;
+    if (!unparsedWordsString.includes(dateString)) {
+      unparsedWordsString += `${dateString}\n`;
+    }
+
+    let startOfNextWordIdx = 0;
+    for (let j = startOfWordIdx; j < mdFileLines.length; j++) {
+      const line = mdFileLines[j];
+      if (numberedEntryRe.test(line) && j !== startOfWordIdx) {
+        startOfNextWordIdx = j;
+        break;
+      }
+
+      unparsedWordsString += `${line}\n`;
+    }
+    i =
+      startOfNextWordIdx === 0
+        ? mdFileLines.length - 1
+        : startOfNextWordIdx - 1;
+  }
+}
+
+const resultPath = path.resolve('output/anki.csv');
+fs.writeFileSync(resultPath, csvString, 'utf-8');
+
+if (unparsedWordsCount > 0) {
+  console.log(`There are ${unparsedWordsCount} unparsed words!`);
+} else {
+  console.log('Everything parsed successfully!');
+}
+
+const unparsedWordsPath = path.resolve('output/unparsed-words.md');
+fs.writeFileSync(unparsedWordsPath, unparsedWordsString, 'utf-8');
---
+++ input/vocab.md
@@ -0,0 +1,303 @@
+# 4 Dec 2022
+
+1)poplar (noun) – тополь
+
+There's also cottonwood
+
+While **poplar may grow to 100 feet high and two feet across, cottonwood can reach to 125 feet and be much larger in diameter**
+
+2)acacia (noun) – акация
+
+3)birch (noun) – берёза
+
+4)fir-tree, fir (noun) – ель, ёлка
+
+5)pine (noun) – сосна
+
+6)maple (noun) – клён
+
+7)oak (noun) – дуб
+
+8)roof (noun)– крыша
+
+9)chimney (noun) – дымоход, труба
+
+10)attic (noun) – чердак
+
+11)front door (noun) – парадная дверь (главный вход в здание), входная дверь
+
+12)porch (noun) – крыльцо (наружная пристройка при входе в дом)
+
+She stood in the porch and rang the doorbell
+
+13)vault (noun) – подвал
+
+14)terrace (noun) – терраса, веранда
+
+15)floor (noun) – этаж / пол
+
+16)diaper (noun) – пелёнка, подгузник
+
+17)fall down (phrasal verb) – упасть
+
+18)rebuild (verb) – перестроить, реконструировать, восстановить
+
+19)probably (adverb) – возможно
+
+# 4 Jan 2023
+
+20)still (adverb) – все еще, еще, до сих пор
+
+21)how (adverb) – как
+
+22)soon (adverb) – вскоре, скоро
+
+23)darling (noun) – дорогой, любимый
+
+24)try (verb) – пытаться
+
+# 6 Jan 2023
+
+25)start a computer (collocation) – запустить компьютер
+
+26)restart (verb) – перезагрузить
+
+27)crash (verb) – выйти из строя
+
+28)upload (verb) – загружать (загружать что-то с компьютера в интернет)
+
+29)post (verb) – размещать что-либо в интернете / постить
+
+# 7 Jan 2023
+
+30)cucumber (noun) – огурец
+
+31)beet (noun) – свекла
+
+32)sweet pepper (noun) – сладкий перец, болгарский перец
+
+33)eggplant (noun) – баклажан
+
+34)onion (noun) – лук, луковица
+
+35)garlic (noun) – чеснок
+
+36)vegetables (noun) – овощи
+
+37)roar (noun) – рев
+
+37)to roar (verb) – рычать
+
+38)hotfix (noun) – исправление
+
+39)finally (adverb) – наконец, наконец-то
+
+40)salad (noun) – салат
+
+41)to slice (verb) – резать
+
+# 8 Jan 2023
+
+42)go ahead (phrasal verb) – взяться за что-то, начинать что-то делать / (чаще всего используется в повелительном наклонении) давай, начинай, валяй, продолжай
+
+Go ahead with your own life and leave me alone. Займись своей жизнью и оставь меня в покое.
+
+Go ahead, tale a closer look. Давай, посмотри поближе.
+
+43)go along with (phrasal verb) – соглашаться с чьим-то мнением или решением, поддержать идею
+
+I can’t go along with this. Я не могу согласиться с этим.
+
+You have to pretend to go along with him. Ты должен притвориться, что согласен с ним.
+
+44)go away (phrasal verb) – а)уходи, проваливай! б)уехать или выбраться куда-то, особенно на выходные
+
+They expect us to go away for the weekend. Они ожидают, что мы уедем куда-нибудь на выходные
+
+45)go back (phrasal verb) – вернуться, возвращаться (если возвращаемся за кем/чем-то, то используем предлог for)
+
+I have to go back for Sarah. Я должен вернуться за Сарой
+
+Go back to work. Возвращайся к работе
+
+46)go by (phrasal verb) – проходить мимо (обычно речь идет о времени или возможностях)
+
+Meanwhile, two days goes by like that. Тем временем, так прошло два дня.
+
+As the years went by. Спустя годы или с течением времени (это фраза)
+
+47)to pretend (verb) – притвориться, делать вид
+
+Pretend you're busy working.
+
+Pretend to be asleep and see what he does.
+
+# 28 Jan 2023
+
+48)pistol, gun (noun) – пистолет
+
+Put the gun on the ground and go away.
+
+Why does he carry a gun?
+
+49)Excuse me! (phrase) – Извините меня!
+
+50)representative (noun) – представитель
+
+51)basement (noun) – подвал
+
+52)penultimate (adjective) – предпоследний
+
+53)chimp, chimpanzees (noun) – шимпанзе
+
+54)stray (adjective) – бездомный (about animals)
+
+55)pants (noun) – штаны
+
+56)chokehold (noun) – удушающий захват
+
+# 29 Jan 2023
+
+57)loaf (noun) – буханка
+
+# 30 Jan 2023
+
+58)trigger (noun) – спусковой крючок
+
+pull the trigger (collocation) – нажать на спусковой крючок
+
+59)flawless (adjective) – безупречный
+
+**flawlessly** ­(adverb) – безупречно
+
+I am sure that my behavior was absolutely flawless
+
+60)prepare meal (collocation) – готовить еду
+
+61)subway (noun) – метро
+
+**ride the subway** – ездить в метро
+
+**Subway** – American metro
+
+synonym - metro
+
+Metro - in the rest of the world
+
+62)nah – нет (видимо сленг)
+
+63)anyone else (phrase) – кто-нибудь еще
+
+64)throat (noun) – горло
+
+# 2 Feb 2023
+
+65)leisure time (phrase) – свободное время (время для отдыха)
+
+66)reconnect with friends (collocation) – восстановить связь с друзьями, снова общаться
+
+67)bumper (noun) – бампер
+
+68)normal people (phrase) – нормальные люди
+
+69)I can't stand something (phrase) – я ненавижу, не могу терпеть
+
+70)bastard (noun) – сволочь
+
+# 8 Feb 2023
+
+71)resolution (noun) –
+
+А) разрешение
+
+Б) решительность
+
+She showed her resolution by not attending the meeting
+
+72)inch (noun) – дюйм, 1 inch = 2.54 cm
+
+Set them just an inch or two deeper than they were in the pot
+
+73)noisy (adjective) – шумный
+
+He **moved on** (**двигался дальше**) through the noisy forest.
+
+The party next door was so noisy that I couldn't sleep.
+
+The street was noisy with traffic during rush hour.
+
+The children were playing and laughing, making the room noisy and cheerful.
+
+74)loud (adjective) – громкий
+
+75)narrow (adjective) – узкий
+
+76)wide (adjective) – широкий
+
+77)straight (adjective) – прямой
+
+# 9 Feb 2023
+
+78)plump (adjective) – полный
+
+I swear, I’m gonna get plump
+
+Mr Reiters pink, plump face flushed scarlet. Пухлое розовое лицо мистера Рейтера вспыхнуло
+
+79)skinny (adjective) – тощий, худой
+
+No, he was too skinny.
+
+Do you want a skinny person?
+
+The child was pale and skinny.
+
+80)old (adjective) – старый
+
+81)young (adjective) – молодой
+
+82)curly (adjective) – кудрявый
+
+Enissa is described as having distinctive large brown eyes and brown curly hair
+
+# 11 Feb 2023
+
+84)fog (noun) – туман
+
+85)foggy (adjective) – туманный
+
+86)cloud (noun) – облако
+
+87)cloudy (adjective) – облачный
+
+88)hail (noun) – град
+
+89)hailstorm (noun) – град
+
+90)puddle (noun) – лужа
+
+91)storm (noun) – шторм, буря, ураган, гроза
+
+92)stormy (adjective) – штормовой
+
+93)wind (noun) – ветер
+
+94)breeze (noun) – легкий ветер
+
+It's so nice to feel a gentle breeze on my cheek
+
+We get a nice breeze from the window, so we don't need to turn on the air conditioning
+
+95)windy (adjective) – ветренный
+
+96)to blow (verb) – дуть
+
+97)big breakfast (phrase) – сытный, плотный завтрак
+
+98)big lunch (phrase) – сытный, плотный обед
+
+# 12 Feb 2023
+
+99)civil (adjective) – гражданский
+
+100)civilian (noun) – гражданский
